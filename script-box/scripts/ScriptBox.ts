b5cba91a0f04ca9ba02c24c89a1243fe1425b70e
Mike Ammerlaan <mammerla@users.noreply.github.com>
2023-09-20 10:02:48 -0700

    Add usage instruction

--- script-box/scripts/ScriptBox.ts
+++ script-box/scripts/ScriptBox.ts
@@ -1,7 +1,7 @@
 import * as mc from "@minecraft/server";
 import * as mcui from "@minecraft/server-ui";
 
-const overworld = mc.world.getDimension("overworld");
+// When running a world with this behavior pack, type 'run' in chat to trigger this code.
 
 export function scriptBox(log: (message: string, status?: number) => void, targetLocation: mc.Vector3) {
   mc.world.sendMessage("Hello world! Replace this line with your code.");
