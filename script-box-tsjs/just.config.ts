e675b3d726cdd50762b7878b036b30eb8931050a
Keyyard <keyyard8888@gmail.com>
2024-09-06 02:14:13 +0700

    added script-box-tsjs

---
+++ script-box-tsjs/just.config.ts
@@ -0,0 +1,74 @@
+import { argv, parallel, series, task, tscTask } from "just-scripts";
+import {
+  bundleTask,
+  BundleTaskParameters,
+  CopyTaskParameters,
+  cleanTask,
+  cleanCollateralTask,
+  copyTask,
+  coreLint,
+  mcaddonTask,
+  setupEnvironment,
+  ZipTaskParameters,
+  STANDARD_CLEAN_PATHS,
+  DEFAULT_CLEAN_DIRECTORIES,
+  getOrThrowFromProcess,
+  watchTask,
+} from "@minecraft/core-build-tasks";
+import path from "path";
+import fs from "fs";
+// Setup env variables
+setupEnvironment(path.resolve(__dirname, ".env"));
+const projectName = getOrThrowFromProcess("PROJECT_NAME");
+
+let entryPointPath = path.join(__dirname, "./scripts/main.ts");
+if (!fs.existsSync(entryPointPath)) {
+  entryPointPath = path.join(__dirname, "./scripts/main.js");
+}
+const bundleTaskOptions: BundleTaskParameters = {
+  entryPoint: entryPointPath,
+  external: ["@minecraft/server", "@minecraft/server-gametest", "@minecraft/server-ui"],
+  outfile: path.resolve(__dirname, "./dist/scripts/main.js"),
+  minifyWhitespace: false,
+  sourcemap: true,
+  outputSourcemapPath: path.resolve(__dirname, "./dist/debug"),
+};
+
+const copyTaskOptions: CopyTaskParameters = {
+  copyToBehaviorPacks: [`./behavior_packs/${projectName}`],
+  copyToScripts: ["./dist/scripts"],
+};
+
+const mcaddonTaskOptions: ZipTaskParameters = {
+  ...copyTaskOptions,
+  outputFile: `./dist/packages/${projectName}.mcaddon`,
+};
+
+// Lint
+task("lint", coreLint(["scripts/**/*.{ts,js}"], argv().fix));
+// Build
+task("typescript", tscTask());
+task("bundle", bundleTask(bundleTaskOptions));
+task("build", series("typescript", "bundle"));
+
+// Clean
+task("clean-local", cleanTask(DEFAULT_CLEAN_DIRECTORIES));
+task("clean-collateral", cleanCollateralTask(STANDARD_CLEAN_PATHS));
+task("clean", parallel("clean-local", "clean-collateral"));
+
+// Package
+task("copyArtifacts", copyTask(copyTaskOptions));
+task("package", series("clean-collateral", "copyArtifacts"));
+
+// Local Deploy used for deploying local changes directly to output via the bundler. It does a full build and package first just in case.
+task(
+  "local-deploy",
+  watchTask(
+    ["scripts/**/*.{ts,js}", "behavior_packs/**/*.{json,lang,png}", "resource_packs/**/*.{json,lang,png}"], // Change this line
+    series("clean-local", "build", "package")
+  )
+);
+
+// Mcaddon
+task("createMcaddonFile", mcaddonTask(mcaddonTaskOptions));
+task("mcaddon", series("clean-local", "build", "createMcaddonFile"));
---
+++ script-box-tsjs/scripts/main.js
@@ -0,0 +1,3 @@
+import { world } from "@minecraft/server";
+
+world.sendMessage("Hello, world! AA");
